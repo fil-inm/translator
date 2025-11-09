87e6f1068dfa2c09549728d2c782e995e02bcbb3
fil_in <mihail0silaev@yandex.ru>
2025-11-09 17:02:25 +0300

    feat: add another lib of tokens

--- tokens.hpp
+++ tokens.hpp
@@ -10,34 +10,44 @@ struct Token {
     enum class Type : uint16_t {
         EndOfFile = 0,
         Identifier,
-        Integer,
-        Float,
-        Char,
+        IntegerLiteral,
+        FloatLiteral,
+        CharLiteral,
 
         LParen, RParen,
         LBrace, RBrace,
         LBracket, RBracket,
         Comma, Semicolon, Dot, Backtick,
 
-        Plus, Minus, Star, Slash, Percent,
+        Plus, Minus, Asterisk, Slash, Percent,
         PlusPlus, MinusMinus,
-        Assign, PlusAssign, MinusAssign, StarAssign, SlashAssign, PercentAssign,
-        Shl, Shr, ShlAssign, ShrAssign,
-        Amp, Pipe, Caret,
+
+        Assign, PlusAssign, MinusAssign, AsteriskAssign, SlashAssign, PercentAssign,
+        ShlAssign, ShrAssign,
+        AmpAssign, PipeAssign, CaretAssign,
+
+        Shl, Shr,
+
+        Ampersand, VerticalBar, Caret,
         AmpAmp, PipePipe,
-        Bang, Tilde,
-        Eq, Ne, Lt, Gt, Le, Ge,
+        Exclamation, Tilde,
+
+        EqualEqual, NotEqual,
+        Less, Greater, LessEqual, GreaterEqual,
 
         KwInt, KwChar, KwBool, KwFloat, KwVoid,
+
         KwClass,
         KwConstructor, KwDestructor,
+
         KwIf, KwElif, KwElse,
         KwWhile, KwFor,
         KwReturn, KwBreak, KwContinue,
+
         KwPrint, KwRead,
+
         KwTrue, KwFalse,
         KwAnd, KwOr,
-        KwNew, KwDelete,
 
         TypeName
     };
@@ -50,86 +60,93 @@ struct Token {
 };
 
 inline std::string tokenTypeName(Token::Type t) {
+    using T = Token::Type;
     switch (t) {
-        case Token::Type::EndOfFile: return "EOF";
-        case Token::Type::Identifier: return "Identifier";
-        case Token::Type::Integer: return "Integer";
-        case Token::Type::Float: return "Float";
-        case Token::Type::Char: return "Char";
+        case T::EndOfFile: return "EOF";
+        case T::Identifier: return "identifier";
+        case T::IntegerLiteral: return "integer literal";
+        case T::FloatLiteral: return "float literal";
+        case T::CharLiteral: return "char literal";
 
-        case Token::Type::LParen: return "(";
-        case Token::Type::RParen: return ")";
-        case Token::Type::LBrace: return "{";
-        case Token::Type::RBrace: return "}";
-        case Token::Type::LBracket: return "[";
-        case Token::Type::RBracket: return "]";
-        case Token::Type::Comma: return ",";
-        case Token::Type::Semicolon: return ";";
-        case Token::Type::Dot: return ".";
-        case Token::Type::Backtick: return "`";
+        case T::LParen: return "(";
+        case T::RParen: return ")";
+        case T::LBrace: return "{";
+        case T::RBrace: return "}";
+        case T::LBracket: return "[";
+        case T::RBracket: return "]";
+        case T::Comma: return ",";
+        case T::Semicolon: return ";";
+        case T::Dot: return ".";
+        case T::Backtick: return "`";
 
-        case Token::Type::Plus: return "+";
-        case Token::Type::Minus: return "-";
-        case Token::Type::Star: return "*";
-        case Token::Type::Slash: return "/";
-        case Token::Type::Percent: return "%";
-        case Token::Type::PlusPlus: return "++";
-        case Token::Type::MinusMinus: return "--";
-        case Token::Type::Assign: return "=";
-        case Token::Type::PlusAssign: return "+=";
-        case Token::Type::MinusAssign: return "-=";
-        case Token::Type::StarAssign: return "*=";
-        case Token::Type::SlashAssign: return "/=";
-        case Token::Type::PercentAssign: return "%=";
-        case Token::Type::Shl: return "<<";
-        case Token::Type::Shr: return ">>";
-        case Token::Type::ShlAssign: return "<<=";
-        case Token::Type::ShrAssign: return ">>=";
-        case Token::Type::Amp: return "&";
-        case Token::Type::Pipe: return "|";
-        case Token::Type::Caret: return "^";
-        case Token::Type::AmpAmp: return "&&";
-        case Token::Type::PipePipe: return "||";
-        case Token::Type::Bang: return "!";
-        case Token::Type::Tilde: return "~";
-        case Token::Type::Eq: return "==";
-        case Token::Type::Ne: return "!=";
-        case Token::Type::Lt: return "<";
-        case Token::Type::Gt: return ">";
-        case Token::Type::Le: return "<=";
-        case Token::Type::Ge: return ">=";
+        case T::Plus: return "+";
+        case T::Minus: return "-";
+        case T::Asterisk: return "*";
+        case T::Slash: return "/";
+        case T::Percent: return "%";
+        case T::PlusPlus: return "++";
+        case T::MinusMinus: return "--";
 
-        case Token::Type::KwInt: return "int";
-        case Token::Type::KwChar: return "char";
-        case Token::Type::KwBool: return "bool";
-        case Token::Type::KwFloat: return "float";
-        case Token::Type::KwVoid: return "void";
-        case Token::Type::KwClass: return "class";
-        case Token::Type::KwConstructor: return "constructor";
-        case Token::Type::KwDestructor: return "destructor";
-        case Token::Type::KwIf: return "if";
-        case Token::Type::KwElif: return "elif";
-        case Token::Type::KwElse: return "else";
-        case Token::Type::KwWhile: return "while";
-        case Token::Type::KwFor: return "for";
-        case Token::Type::KwReturn: return "return";
-        case Token::Type::KwBreak: return "break";
-        case Token::Type::KwContinue: return "continue";
-        case Token::Type::KwPrint: return "print";
-        case Token::Type::KwRead: return "read";
-        case Token::Type::KwTrue: return "true";
-        case Token::Type::KwFalse: return "false";
-        case Token::Type::KwAnd: return "and";
-        case Token::Type::KwOr: return "or";
-        case Token::Type::KwNew: return "new";
-        case Token::Type::KwDelete: return "delete";
+        case T::Assign: return "=";
+        case T::PlusAssign: return "+=";
+        case T::MinusAssign: return "-=";
+        case T::AsteriskAssign: return "*=";
+        case T::SlashAssign: return "/=";
+        case T::PercentAssign: return "%=";
+        case T::ShlAssign: return "<<=";
+        case T::ShrAssign: return ">>=";
+        case T::AmpAssign: return "&=";
+        case T::PipeAssign: return "|=";
+        case T::CaretAssign: return "^=";
 
-        case Token::Type::TypeName: return "<type-name>";
+        case T::Shl: return "<<";
+        case T::Shr: return ">>";
+
+        case T::Ampersand: return "&";
+        case T::VerticalBar: return "|";
+        case T::Caret: return "^";
+        case T::AmpAmp: return "&&";
+        case T::PipePipe: return "||";
+        case T::Exclamation: return "!";
+        case T::Tilde: return "~";
+
+        case T::EqualEqual: return "==";
+        case T::NotEqual: return "!=";
+        case T::Less: return "<";
+        case T::Greater: return ">";
+        case T::LessEqual: return "<=";
+        case T::GreaterEqual: return ">=";
+
+        case T::KwInt: return "int";
+        case T::KwChar: return "char";
+        case T::KwBool: return "bool";
+        case T::KwFloat: return "float";
+        case T::KwVoid: return "void";
+        case T::KwClass: return "class";
+        case T::KwConstructor: return "constructor";
+        case T::KwDestructor: return "destructor";
+        case T::KwIf: return "if";
+        case T::KwElif: return "elif";
+        case T::KwElse: return "else";
+        case T::KwWhile: return "while";
+        case T::KwFor: return "for";
+        case T::KwReturn: return "return";
+        case T::KwBreak: return "break";
+        case T::KwContinue: return "continue";
+        case T::KwPrint: return "print";
+        case T::KwRead: return "read";
+        case T::KwTrue: return "true";
+        case T::KwFalse: return "false";
+        case T::KwAnd: return "and";
+        case T::KwOr: return "or";
+
+
+        case T::TypeName: return "<type-name>";
     }
     return "<unknown>";
 }
 
 inline std::string Token::toString() const {
-    return tokenTypeName(type) + " '" + lexeme + "' @" + std::to_string(pos.line) + ":" + std::to_string(pos.column);
+    return tokenTypeName(type) + " '" + lexeme + "' @" +
+           std::to_string(pos.line) + ":" + std::to_string(pos.column);
 }
-
