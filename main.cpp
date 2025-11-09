6bd1bf8f5f48842f1805b782adc5bd417140b982
fil_in <mihail0silaev@yandex.ru>
2025-11-09 17:02:58 +0300

    feat: add beauty cheks for code

--- main.cpp
+++ main.cpp
@@ -1,89 +1,33 @@
 #include "lexer.hpp"
+#include "parser.hpp"
 #include <iostream>
-#include <fstream>
-#include <iomanip>
 #include <vector>
-
-std::string tokenTypeToString(TokenType type) {
-    switch (type) {
-        case TokenType::KEYWORD:          return "KEYWORD";
-        case TokenType::IDENTIFIER:       return "IDENTIFIER";
-        case TokenType::TYPE_NAME:        return "TYPE_NAME";
-
-        case TokenType::NUMBER:           return "NUMBER";
-        case TokenType::STRING_LITERAL:   return "STRING_LITERAL";
-        case TokenType::CHAR_LITERAL:     return "CHAR_LITERAL";
-        case TokenType::BOOL_LITERAL:     return "BOOL_LITERAL";
-
-        case TokenType::OPERATOR_PLUS:    return "OPERATOR_PLUS";
-        case TokenType::OPERATOR_MINUS:   return "OPERATOR_MINUS";
-        case TokenType::OPERATOR_MUL:     return "OPERATOR_MUL";
-        case TokenType::OPERATOR_DIV:     return "OPERATOR_DIV";
-        case TokenType::OPERATOR_ASSIGN:  return "OPERATOR_ASSIGN";
-        case TokenType::OPERATOR_EQ:      return "OPERATOR_EQ";
-        case TokenType::OPERATOR_NEQ:     return "OPERATOR_NEQ";
-        case TokenType::OPERATOR_LT:      return "OPERATOR_LT";
-        case TokenType::OPERATOR_GT:      return "OPERATOR_GT";
-        case TokenType::OPERATOR_LE:      return "OPERATOR_LE";
-        case TokenType::OPERATOR_GE:      return "OPERATOR_GE";
-        case TokenType::OPERATOR_AND:     return "OPERATOR_AND";
-        case TokenType::OPERATOR_OR:      return "OPERATOR_OR";
-        case TokenType::OPERATOR_NOT:     return "OPERATOR_NOT";
-        case TokenType::OPERATOR_INC:     return "OPERATOR_INC";
-        case TokenType::OPERATOR_DEC:     return "OPERATOR_DEC";
-        case TokenType::OPERATOR_SCOPE:  return "OPERATOR_SCOPE";
-        case TokenType::OPERATOR_MOD:     return "OPERATOR_MOD";
-
-        case TokenType::DELIM_SEMICOLON:  return "DELIM_SEMICOLON";
-        case TokenType::DELIM_COMMA:      return "DELIM_COMMA";
-        case TokenType::DELIM_DOT:        return "DELIM_DOT";
-        case TokenType::DELIM_LPAREN:     return "DELIM_LPAREN";
-        case TokenType::DELIM_RPAREN:     return "DELIM_RPAREN";
-        case TokenType::DELIM_LBRACE:     return "DELIM_LBRACE";
-        case TokenType::DELIM_RBRACE:     return "DELIM_RBRACE";
-        case TokenType::DELIM_LBRACKET:   return "DELIM_LBRACKET";
-        case TokenType::DELIM_RBRACKET:   return "DELIM_RBRACKET";
-        case TokenType::DELIM_COLON:     return "DELIM_COLON";
-
-        case TokenType::END_OF_FILE:      return "END_OF_FILE";
-        default:                          return "UNKNOWN";
-    }
-}
+#include <string>
 
 int main() {
-    Lexer lexer("input.txt");
-    lexer.loadKeywordsFromFile("keywords.txt");
+    const std::string keywordsFile = "keywords.txt";
 
-    std::vector<Token> tokens;
+    std::vector<std::string> testFiles = {
+        "Correct1.txt",
+        "Correct2.txt",
+        "Correct3.txt",
+        "Incorrect1.txt",
+        "Incorrect2.txt",
+        "Incorrect3.txt",
+        "Incorrect4.txt",
+        "Incorrect5.txt",
+    };
 
-    while (true) {
-        Token token = lexer.nextLexem();
-        if (token.type == TokenType::END_OF_FILE)
-            break;
-        tokens.push_back(token);
+    for (const auto& sourceFile : testFiles) {
+        std::cout << "=== Проверяю: " << sourceFile << " ===\n";
+
+        Lexer lexer(sourceFile, keywordsFile);
+        Parser parser(lexer);
+
+        if (parser.parseProgram()) {
+            std::cout << "Разбор завершён успешно\n\n";
+        }
     }
 
-    std::ofstream out("tokens.txt");
-    if (!out.is_open()) {
-        std::cerr << "Cannot open tokens.txt for writing.\n";
-        return 1;
-    }
-
-    out << "---------------------------------\n";
-    out << "| #   | TOKEN TYPE        | VALUE\n";
-    out << "---------------------------------\n";
-
-    int index = 1;
-    for (const auto& t : tokens) {
-        out << "| "
-            << std::setw(3) << index++ << " "
-            << "| " << std::left << std::setw(17) << tokenTypeToString(t.type)
-            << "| " << std::left << std::setw(12) << t.value <<"\n";
-    }
-
-    out << "--------------------------------\n";
-    out.close();
-
-    std::cout << "Tokens written successfully!\n";
     return 0;
 }
